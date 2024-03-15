de75b9b9b59902493725592f75702209c22c79d4
santhoshXD <santhoshkm416@gmail.com>
2024-03-15 19:43:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import { createGlobalStyle } from "styled-components";
+import ProductDetails from "./components/ProductDetails";
+import BuyTshirts from "./components/BuyTshirts";
+import WomenTshirts from "./components/WomenTshirt";
+import MenTshirts from "./components/MenTshirt";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import { createContext, useContext, useState } from "react";
+ 
+
+
+const GlobalStyles = createGlobalStyle`
+body{
+
+  button{
+    cursor: pointer;
+  }
+
+
+  font-family: "Dosis", sans-serif;
+  overflow-y: auto;
+
+ 
+::-webkit-scrollbar {
+  width: 12px;  
+}
+
+ 
+::-webkit-scrollbar-track {
+  background: white;  
+}
+
+ 
+::-webkit-scrollbar-thumb {
+  background: lightgrey;  
+  border-radius: 6px; 
+}
+
+ 
+::-webkit-scrollbar-thumb:hover {
+  background: darkgrey;  
+  cursor: pointer;
+}
+
+
+ }
+
+
+`
+
+const CartContext = createContext();
+
+export function useCart() {
+   return useContext(CartContext)
+}
+ 
 
 function App() {
+
+
+  const[cart,setCart] = useState([])
+
+
+  const addToCart = (item) =>{
+    setCart([...cart,item])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <GlobalStyles/>
+  <CartContext.Provider value={{cart,addToCart}} >
+
+  <BrowserRouter>
+  <Routes>
+  <Route path="/home" element={<HomePage />} />
+  <Route path="/shop" element={<BuyTshirts />} />
+  <Route path="/women-tshirts" element={<WomenTshirts />} />
+  <Route path="/men-tshirts" element={<MenTshirts />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/contact" element={<Contact />} />
+  <Route path="/shop/product/:id" element={<ProductDetails />} />
+  <Route path="/" element={<LoginPage/>}/>
+  </Routes>
+  </BrowserRouter>
+  </CartContext.Provider>
+ 
+  </>
   );
 }
 
